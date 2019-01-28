90f230941d633881e3c3f81b7608c9a91a91cf1e
Christopher Kalfas <chriskalfas@yahoo.com>
2019-01-28 18:50:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,9 +13,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
  return array
 }
 
-function addElementToEndOfArray (array, element){
-  return [array, ...element]
-}
+
 
 function destructivelyAddElementToEndOfArray(array, element){
   [array.push(element)]
