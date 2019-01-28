1f37f3795b353e57ed88a8dfc32a4ae8a034cd32
Christopher Kalfas <chriskalfas@yahoo.com>
2019-01-28 17:12:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,7 @@
 var chocolateBars =["snickers", "hundred grand", "litkat", "skittles"];
 
+chocolateBars.unshift("butter fingers")
+
 function addElementToBeginingOfArray(array, element){
   return chocolateBars.unshift("butter fingers")
 }
