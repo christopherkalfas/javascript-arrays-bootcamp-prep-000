9b7d46c138c541dff79e2f40945fec03b476a1a0
Christopher Kalfas <chriskalfas@yahoo.com>
2019-01-28 18:29:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,6 +10,6 @@ function addElementToBeginningOfArray(array, element){
 
 function destructivelyAddElementToBeginningOfArray(array, element){
  [array.unshift(element)]
- return [array]
+ return [element, ...array]
 }
 
