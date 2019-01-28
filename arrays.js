7a47fc5ebeb6730074c4c83072a49f636f03c159
Christopher Kalfas <chriskalfas@yahoo.com>
2019-01-28 19:21:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,6 +27,6 @@ function accessElementInArray(array, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-   array.unshift()
+   array.unshift(element)
    return array
 }
